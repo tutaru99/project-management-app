5bdd9f73274498ac138428d1f6b9149ed5ae96cb
Simas <simas.raudonis@gmail.com>
2021-02-16 17:50:54 +0200

    Preparing work Env

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -8,6 +8,7 @@
 export default {};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+
+<style lang="scss" scoped>
+
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,18 +3,18 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>Home Page View</h1>
     <h1>Project Management Application</h1>
-    <HelloWorld/>
+    <HomeComponent/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomeComponent from '@/components/HomeComponent.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HomeComponent
   }
 }
 </script>
