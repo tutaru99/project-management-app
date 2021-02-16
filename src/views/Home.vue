6e7d27359c13815e6b50f86bc64d00a9d10e450e
Simas <simas.raudonis@gmail.com>
2021-02-16 17:41:39 +0200

    GitTest

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <h1>Home Page View</h1>
+    <h1>Project Management Application</h1>
     <HelloWorld/>
   </div>
 </template>
