14d596a8ee2934fb61324781153ed394b1bb71ce
Simas <simas.raudonis@gmail.com>
2021-02-16 17:37:45 +0200

    Project Init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home Page View</h1>
+    <HelloWorld/>
   </div>
 </template>
 
